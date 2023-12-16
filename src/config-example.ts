30c294010a21e919992c9c94176628c30ebff45e
Angel <22824872+Zyplos@users.noreply.github.com>
2023-12-15 21:48:55 -0600

    added icons to config

--- src/config-example.ts
+++ src/config-example.ts
@@ -28,12 +28,26 @@ type Config = {
   detailedStatusId: string;
   summariedSectionIds: string[];
   projectBoardLink: string;
-
-  discordBoardChannelId: string;
-  discordBoardMessageId?: string;
   discordBoardTitle?: string;
 
   timelineOnly?: boolean;
+
+  discordBoardChannelId: string;
+  discordBoardMessageId?: string;
+  discordIcons: {
+    INPROGRESS: Record<string, string>;
+    STARTPOINT_TOP: string;
+    STARTPOINT_BOTTOM: string;
+    MIDPOINT_TOP: string;
+    MIDPOINT_BOTTOM: string;
+    ENDPOINT_TOP: string;
+    ENDPOINT_BOTTOM: string;
+
+    BLANK: string;
+
+    UPCOMING: string;
+    COMPLETE: string;
+  };
 };
 
 export default {
@@ -53,10 +67,27 @@ export default {
   projectBoardLink: "https://github.com/orgs/zyplos/projects/5",
   discordBoardTitle: "☀️ Spring 2024",
 
-  // if you'd only like to use the timeline stuff, set this to true
-  // you can leave the things above this as empty strings
   timelineOnly: true,
 
   discordBoardChannelId: "520697468300689409",
   discordBoardMessageId: "1184624444119986297",
+
+  discordIcons: {
+    INPROGRESS: {
+      1: "<a:tlInProgress1:1185425735394000946>",
+      2: "<a:tlInProgress2:1185425736794918942>",
+      3: "<a:tlInProgress3:1185425737533104269>",
+    },
+    STARTPOINT_TOP: "<:tlTickerStartTop:1185345737052147784>",
+    STARTPOINT_BOTTOM: "<:tlTickerStartBottom:1185345586237550694>",
+    MIDPOINT_TOP: "<:tlTickerMidTop:1185424952174850248>",
+    MIDPOINT_BOTTOM: "<:tlTickerMidBottom:1185424950920761504>",
+    ENDPOINT_TOP: "<:tlTickerEndTop:1185345583679021076>",
+    ENDPOINT_BOTTOM: "<:tlTickerEndBottom:1185345582848553110>",
+
+    BLANK: "<:__:1053765895220101180>",
+
+    UPCOMING: "<:tlUpcoming:1185345683742527538>",
+    COMPLETE: "<:tlComplete:1185422176313151538>",
+  },
 } as Config;
